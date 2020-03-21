370a8161a52a7ce3ac4dae8168ce8f4242f503d0
zhenmai <zhenmaihu2017@gmail.com>
2020-03-21 00:16:14 -0700

    modified the generate test data to generate the data which is more realistic (generate more links in specific range)

--- GenerateTestData.cpp
+++ GenerateTestData.cpp
@@ -1,61 +1,73 @@
 #include <iostream>
+#include <vector>   // std::vector
 #include <fstream>  // std::ofstream
 #include <random> 	// std::rand, std::srand, std::default_random_engine
 #include <algorithm> // std::generate
 
-// from benchmarking.cpp in class
-template < typename RandomGenerator >
-	auto build_rand_vec( RandomGenerator gen, size_t const size )
-	{
-		// see timing.hpp for a description of decltype().
-		// Initialises a vector with the appropriate size, in which we will create the random data.
-		std::vector< decltype((gen)()) > random_data( size );
+// Generate a integer from min_num to max_num with uniform distribution
+int numGenerator(double min_num, double max_num) 
+{
+   std::random_device rand_dev;
+   std::mt19937 generator(rand_dev());
+   std::uniform_int_distribution<int> distr(0, max_num);
+   return distr(generator);
+}
 
-		// std::generate() = STL. This could be done with a loop from 0,...,size, calling gen() each
-		// iteration. See https://www.fluentcpp.com/2017/01/05/the-importance-of-knowing-stl-algorithms/
-		// for a compelling discussion of why using STL algorithms like this is better than explicit loops.
-		// Spoiler: it better self-documents (in code) intent and is less prone to off-by-one-errors.
-		// For an explanation of syntax, see the use of std::for_each() in timing.hpp
-		std::generate( random_data.begin(), random_data.end(), gen );
-
-		return random_data;
-	}
+// Generate a integer from min_num to max_num with normal distribution
+int  numNorGenerator(double min_num, double max_num) 
+{
+   std::random_device rand_dev;
+   std::mt19937 generator(rand_dev());
+   double mean = 0.5 * (max_num-min_num) + min_num;
+   double stddev = 0.2 * (max_num-min_num);
+   std::normal_distribution<double> distribution(mean, stddev);
+   double ret;
+   do
+   {
+      ret = distribution(generator);
+   } while((ret<min_num)||(ret>=max_num));
+   return int(ret);
+}
 
 int main (int argc, char *argv[])
 {
-	if(argc == 4)
+	if(argc == 3)
 	{
-		unsigned num_vertexes = std::stoi(argv[1]);
-		unsigned test_size = std::stoi(argv[2]) * 2;
-		std::ofstream myfile (argv[3]);
-
-		if(num_vertexes == 0 || test_size == 0 || !myfile.is_open())
-		{
-			std::cout << "Invalid Input! " << std::endl;
-			return 0;
-		}
-
-		if (myfile.is_open())
-	 	{
-	  	 	myfile << num_vertexes << "\n";
-
-	  	 	auto output = build_rand_vec([](){return static_cast< unsigned >( std::rand());}, test_size);
-															
-	  	 	for(unsigned i = 0; i < output.size(); i++)
-	  	 	{
-	  	 		// output the size of random generation data between [0, num_vertexes)
-	  	 		myfile << output[i] % num_vertexes << ' ';
-	  	 	}
-	  	  	myfile.close();
-		}
-  	}
-  	else
-  	{
-  		std::cout << "Invalid Input: " << std::endl;
-  		std::cout << "Please input the number of vertexed wanted in argc[1]" << std::endl;
-  		std::cout << "Please input the size of graph links wanted in argc[2]" << std::endl;
-  		std::cout << "Please input the output text file name wanted in argc[3]" << std::endl;
-  	}
-  
-  	return 0;
+      int num_edges = 0, cur_edges = 0;
+      int num_vertices = atoi(argv[1]);
+      num_edges = atoi(argv[2]);
+      // matrix to store flags of edges(already exist/empty), so that we can avoid repeating edges
+      std::vector<std::vector<char>> matrix(num_vertices, std::vector<char>(num_vertices,0));
+      while(cur_edges < num_edges) 
+      {
+      	// x are sources which are randomly generated
+      	// y are destinations, some of which are generated with normal distribution
+        int x = numGenerator(0, num_vertices-1);
+        int y;
+        // Let 20% of links generated in a specific area
+        if( cur_edges < (num_edges/5) )
+        {
+          y = numNorGenerator(0.0, double(num_vertices-1));
+        } 
+        else
+        {
+           y = numGenerator(0.0, double(num_vertices-1));
+        }
+        if((x != y) && (matrix[x][y] == 0))
+        {
+           matrix[x][y] = 1;
+           ++cur_edges;
+           std::cout<< x << " " << y <<'\n';
+        }
+      }
+   } 
+   else 
+   {
+      std::cerr<< "Command line not valid, please follow: \n"
+               << "programName verticesNum edgesNum\n"
+               << "Example: \n"
+               << "./generator 100 1000 >output.txt\n";
+   }
+  return 0;
 }
+
